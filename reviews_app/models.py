4a16769cb80fe6d061cfd89818ce941833e81fa2
Jonas Stiefer <neo.stief.jon@gmail.com>
2025-08-01 18:12:53 +0200

    add Review model

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Review(models.Model):
+    RATING_CHOICES = [(i, str(i)) for i in range(1, 11)]
+
+    business_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='business_reviews')
+    reviewer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(
+        choices=RATING_CHOICES,
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        help_text="Rating from 1 to 10, where 1 is the worst and 10 is the best."
+    )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(rating__gte=1, rating__lte=10),
+                name='rating_between_1_and_10'
+            )
+        ]
